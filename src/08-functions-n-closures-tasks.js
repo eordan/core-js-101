a8a1afd4ecbb35d7e8861d790e90e42b61eb5bd8
eordan <pistsouandrey@gmail.com>
2023-06-12 23:59:27 +0300

    feat: implement logger function

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -158,8 +158,19 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function log(...args) {
+    const argString = args.map((arg) => JSON.stringify(arg)).join(',');
+
+    const logStart = `${func.name}(${argString}) starts`;
+    const logEnd = `${func.name}(${argString}) ends`;
+
+    logFunc(logStart);
+    const result = func(...args);
+    logFunc(logEnd);
+
+    return result;
+  };
 }
 
 /**
